433b5d9fa8ce759b0c5c9f5be7e4fe1d09b6f768
冰洁  杨 <3307752103@qq.com>
2018-02-04 17:00:33 +0800

    init

---
+++ Leaf/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  Leaf
+//
+//  Created by 冰洁  杨 on 2018/2/4.
+//  Copyright © 2018年 冰洁  杨. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class CircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Leaf/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Leaf
+//
+//  Created by 冰洁  杨 on 2018/2/3.
+//  Copyright © 2018年 冰洁  杨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+
+
+}
+
