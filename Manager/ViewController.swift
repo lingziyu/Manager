15dd904d1531bc754b63f4f9091e778585d130b5
冰洁  杨 <3307752103@qq.com>
2018-03-08 11:37:28 +0800

    add tab bar controller

--- Manager/ViewController.swift
+++ Manager/ViewController.swift
@@ -10,13 +10,9 @@ import UIKit
 import Charts
 
 class ViewController: UIViewController {
- var numbers : [Double] = []
-    @IBAction func btnbutton(_ sender: Any) {
-        let input  = Double(txtTextBox.text!) //gets input from the textbox - expects input as double/int
-        numbers.append(input!) //here we add the data to the array.
-        updateGraph()
-    }
-    @IBOutlet weak var txtTextBox: UITextField!
+    var numbers : [Double] = []
+    var time: [Double] = []
+    var chartTitle: String? = "温度"
  
 
     @IBOutlet weak var chtChart: LineChartView!
@@ -24,6 +20,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        numbers.append(37)
+        numbers.append(38)
+        numbers.append(32)
+        numbers.append(30)
+        numbers.append(25)
+        numbers.append(20)
+        numbers.append(22)
+        numbers.append(23)
+       
+        updateGraph()
         
     }
     func updateGraph(){
@@ -36,16 +42,25 @@ class ViewController: UIViewController {
             let value = ChartDataEntry(x: Double(i), y: numbers[i]) // here we set the X and Y status in a data chart entry
             lineChartEntry.append(value) // here we add it to the data set
         }
+        if chartTitle != nil{
+            let line1 = LineChartDataSet(values: lineChartEntry, label: chartTitle) //Here we convert lineChartEntry to a LineChartDataSet
+            line1.setCircleColor(UIColor(red: 0x86/255, green: 0xbe/255, blue: 0xbb/255, alpha: 1))
+            line1.circleHoleColor = UIColor.black
+            line1.colors = [UIColor(red: 0x86/255, green: 0xbe/255, blue: 0xbb/255, alpha: 1)] //Sets the colour
+            line1.drawCircleHoleEnabled = false
+
+            chtChart.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
+            chtChart.chartDescription?.enabled = false
+            chtChart.rightAxis.enabled = false
+            let data = LineChartData() //This is the object that will be added to the chart
+            data.setDrawValues(true)
+            
+            data.addDataSet(line1) //Adds the line to the dataSet
+      
         
-        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number") //Here we convert lineChartEntry to a LineChartDataSet
-        line1.colors = [NSUIColor.blue] //Sets the colour to blue
-        
-        let data = LineChartData() //This is the object that will be added to the chart
-        data.addDataSet(line1) //Adds the line to the dataSet
-        
-        
-        chtChart.data = data //finally - it adds the chart data to the chart and causes an update
-        chtChart.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
+            chtChart.data = data //finally - it adds the chart data to the chart and causes an update
+//        chtChart.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
+              }
     }
     
     
